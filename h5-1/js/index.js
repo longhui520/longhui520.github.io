1706993cbca9428abdeaa12ccfee4b7b59693cda
longhui520 <39285837+longhui520@users.noreply.github.com>
2018-06-15 12:01:46 +0800

    mdodiy

--- h5-1/js/index.js
+++ h5-1/js/index.js
@@ -2,11 +2,19 @@ $(function(){
     $('#fullpage').fullpage({
         verticalCentered: false,
         navigation:false,
-        afterLoad:function(link,index){
-            $('.section').eq(index-1).addClass('now')
-        },
+        scrollingSpeead:700,
+        // afterLoad:function(link,index){
+        //     $('.section').eq(index-1).addClass('now')
+        // },
+        // onLeave:function(index,nextIndex,direction){
+        //     $('.section').eq(index-1).removeClass('now')
+        // }
         onLeave:function(index,nextIndex,direction){
-            $('.section').eq(index-1).removeClass('now')
-        }
+            $('.section').eq(index-1).addClass('leave')
+        },
+        ollingSpeead:700,
+         afterLoad:function(link,index){
+            $('.section').eq(index-2).removeClass('leave')
+         }
     })
 })
